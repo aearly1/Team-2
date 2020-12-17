06e06b9fd3fc7cd34a594f89ff5ab40619718e2d
Sohob <therage_11@hotmail.com>
2020-12-17 16:27:15 +0200

    ObjectId type is used instead

--- models/faculty.js
+++ models/faculty.js
@@ -3,7 +3,7 @@ const FacultySchema = new mongoose.Schema(
     {
     facultyName: { type: String, required: true,
     default: "New Faculty",},
-    departments: Array //contains department ids
+    departments: [mongoose.Types.ObjectId] //contains department ids
     }
     );
 module.exports = mongoose.model("Faculty", FacultySchema);
--- models/staffMembers.js
+++ models/staffMembers.js
@@ -12,9 +12,9 @@ const staffSchema = new schema ({
     departmentName: String, //null for HR or just set to HR
     attendance: Array, //should contain JS objects that look like this : {day:01,month:09, year:2020, [ {signed in: 7:00, signed out: 9:00},{ signed in: 11:00, signed out: 13:00}]}
     courses: [String], //array with course ids of courses they teach && empty list in case of HR
-    scheduleSlots: [Number], //can be an array of slot models (nested models) //null in case of HR
-    sentRequests: [Number], //stores request models sent by this particular staff member
-    receivedRequests: [Number], //stores request models submitted to this particular staff
+    scheduleSlots: [mongoose.Types.ObjectId], //can be an array of slot models (nested models) //null in case of HR
+    sentRequests: [mongoose.Types.ObjectId], //stores request models sent by this particular staff member
+    receivedRequests: [mongoose.Types.ObjectId], //stores request models submitted to this particular staff
     annualLeaves: Number,
     Salary: Number
 })
