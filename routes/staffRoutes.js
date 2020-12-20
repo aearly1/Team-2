d4fbd6091cf8af062595479234f3e68bb07bf186
Mostafa Abdelnasser <therage_11@hotmail.com>
2020-12-20 17:58:58 +0200

    Function folder to use implemented funtions

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -9,6 +9,8 @@ const app= express.Router();
 const blacklist = auth.blacklist
 const key = auth.key
 const { body, validationResult, check } = require('express-validator');
+const missinghrs = require('../functions/funcs').missinghours
+const missingdays = require('../functions/funcs').missingdays
 //connectDB()
 //.then(async()=>{
     
@@ -206,89 +208,14 @@ const { body, validationResult, check } = require('express-validator');
     app.get('/missingdays',async(req,res)=>{
         const payload = jwt.verify(req.header('auth-token'),key);
         let u = await staffMembers.findOne({email:payload.email});
-        let dayoff = u.dayOff
-        let today = new Date()
-        let month = today.getMonth()
-        if(today.getDate()>10)
-        month++;
-        let curr = new Date()
-        let mont = curr.getMonth()
-        if(curr.getDate()>10)
-        mont++
-        let max = new Date()
-        max.setMonth(mont,10)
-        max.setHours(19)
-        let min = new Date()
-        min.setMonth(mont-1,11)
-        min.setHours(6)
-        const missingdays= []
-        let att = u.attendance.filter(function(elem){
-            let d = elem.time
-            return elem.op == "sign out" && d.getTime()>min.getTime() && d.getTime() < max.getTime() 
-        })
-        switch (u.dayOff) {
-            case "Saturday":
-                dayoff=6
-                break;
-            case "Sunday":
-                dayoff=0
-            case "Monday":
-                dayoff=1
-                break;
-            case "Tuesday":
-                dayoff=2
-                break;
-            case "Wednesday":
-                dayoff=3
-                break;
-            case "Thursday":
-                dayoff=4
-                break;
-            case "Friday":
-                dayoff=5
-                break;
-        }
-        for (let i = min; i.getTime() <= max.getTime(); i.setTime(i.getTime()+86400000)) {
-            //let ds = new Date()
-            //ds.setDate(i);
-            if(dayoff==i.getDay())
-                continue;
-            if(i.getDay()==5)
-                continue;
-            let fil = att.filter(function(elem){
-                return elem.time.getDate()==i.getDate()
-            })
-            if(fil.length==0)
-            if(i.getTime()<=today.getTime())
-            missingdays.push(i.getDate()+"/"+(i.getMonth()+1))
-        }
         
-        res.send(missingdays)
+        res.send(missingdays(u))
 
     })
     app.get('/missinghours',async(req,res)=>{
         const payload = jwt.verify(req.header('auth-token'),key);
         let u = await staffMembers.findOne({email:payload.email});
-        let sum = 0
-        let curr = new Date()
-        let month = curr.getMonth()
-        if(curr.getDate()>10)
-        month++
-        let max = new Date()
-        max.setMonth(month,10)
-        max.setHours(19)
-        let min = new Date()
-        min.setMonth(month-1,11)
-        min.setHours(6)
-        let att = u.attendance.filter(function(elem){
-            let d = elem.time
-            return elem.op == "sign out" && d.getTime()>min.getTime() && d.getTime() < max.getTime()
-        })
-        att.forEach(element => {
-            sum+=element.net
-        });
-        let mins = ((sum/60)-Math.floor(sum/60))*60
-        res.send(Math.ceil(sum/60)+" Hours "+Math.round(mins) +" Mins")
+        res.send(missinghrs(u))
     })
 
     
