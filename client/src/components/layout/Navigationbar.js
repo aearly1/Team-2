3fbd6c56c82271eb4fc14ee10486ef824ff4e8f5
abdrahmandiab <37490334+abdrahmandiab@users.noreply.github.com>
2021-01-03 23:36:35 +0200

    Diab editted Navbar a tiny bit, added props to HODEditCourse

--- client/src/components/layout/Navigationbar.js
+++ client/src/components/layout/Navigationbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Nav, Navbar} from 'react-bootstrap';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Styles = styled.div`
   .navbar { background-color: #0C0A3E ;
@@ -23,19 +24,32 @@ const Styles = styled.div`
     right: 25%;
   }
 `;
-
-const NavigationBar = () => (
+ //Currently
+function NavigationBar (props) {
+  return(
   <Styles>
     <Navbar>
-      <Navbar.Brand href="/"> Team 2 University System</Navbar.Brand>
+      {props.isLoggedIn?(<Navbar.Brand href="/"> Team 2 University System</Navbar.Brand> ):(<Navbar.Brand href="/login"> Team 2 University System</Navbar.Brand>) }  
+      
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-       <Nav className="ml-auto">
-       <Nav.Item className = "border border-1 border-warning mr-2" style={{borderRadius: 8}}><Nav.Link href="/login">Login</Nav.Link></Nav.Item> 
-          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
-          <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
-        </Nav>
+      <Nav className="ml-auto">
+        {props.isLoggedIn?(<Nav.Item><Nav.Link href="/">{props.name}</Nav.Link></Nav.Item> ):(<Nav.Item className = "border border-1 border-warning mr-2" style={{borderRadius: 8}}><Nav.Link href="/login">Login</Nav.Link></Nav.Item>) }
+        <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+
+      </Nav>
       
     </Navbar>
   </Styles>
-)
+  )
+}
+NavigationBar.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  name: PropTypes.string
+}
+
+NavigationBar.defaultProps = {
+  isLoggedIn: false,
+  name: "Hassan Soubra",
+};
+
 export default NavigationBar
--- client/src/components/pages/general/About.js
+++ client/src/components/pages/general/About.js
@@ -2,23 +2,22 @@ import React from 'react'
 import {Container} from 'react-bootstrap'
 const About = () => {
     return (
-        <div>
+        <div style={{textAlign:"center"}}>
             <h1>About This App:</h1>
             <p className="my-1">
-                This is a full-stack React App for GUC Advanced Computer Lab Course, 
+                This is a MERN-stack App created for GUC Advanced Computer Lab Course Project, 
                 Created by Team 2.
             </p>
-            <p>
-                <strong>Contributing Team 2 Members: </strong>
-                <Container fluid className = "pl-5">    
-                    <ul>
+            <p style ={{paddingTop:30}}>
+                <strong style = {{fontSize:22}}>Contributing Team 2 Members: </strong>
+                 
+                    <ul style={{listStyleType:"none"}}>
                         <li>{"Abdelrahman Diab  --  43-11809"}</li>
                         <li>{"Ali Elbadry  --  43-6833"}</li>
                         <li>{"Mayaaaaaar  --  43-6645"}</li>
                         <li>{"Mostafa Sohob  --  43-8530"}</li>
                         <li>{"Saeed Ashraf  --  43-8342"}</li>
                     </ul>
-                </Container>
             </p>
         </div>
     )
