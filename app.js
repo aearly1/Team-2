77faaa638528978cef498bfac0547212b944b7a1
Mostafa Abdelnasser <therage_11@hotmail.com>
2021-01-15 23:05:38 +0200

    ass

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ connectDB();
 
 //Init Middleware
 app.use(express.json({extended: false}));
-app.use(cors())
+//app.use(cors())
 //Define Routes
 app.use("/api/init", require("./DBInit"));
 app.use("/api/login", require("./routes/login"))
@@ -20,6 +20,17 @@ app.use("/api/hr", require("./routes/HRRoutes"));
 app.use("/api/academicMember",require("./routes/academicMemberRoutes"))
 app.use("/api/coordinator", require("./routes/courseCoordinatorRoutes"));
 app.use("/api/instructor", require("./routes/courseInstructorRoute"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+  }
+  
 
 const PORT = process.env.PORT || 5000;
 
